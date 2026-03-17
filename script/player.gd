f195c13e468cdf0ba25e42a4bd765dadfb4ac80d
monn <pphan@paragoniu.edu.kh>
2026-03-17 14:43:18 +0700

    fix dmg bug for player

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 const TURN_SPEED = 0.05
 const KNIFE_RANGE = 2.0
 
@@ -39,53 +38,65 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		rotate_y(-TURN_SPEED)
 
-	if Input.is_action_pressed("ui_accept"):
-		if ui_script.can_shoot:
-			shoot()
+	if Input.is_action_pressed("ui_accept") and ui_script.can_shoot:
+		shoot()
 
 	move_and_slide()
 
 func shoot():
+	var target = null
+
+	if ray.is_colliding():
+		target = ray.get_collider()
+
 	match Global.current_weapon:
 		"gun":
 			sound_player.stream = preload("res://sound/gun.ogg")
-			sound_player.play()
-			if ray.is_colliding() and ray.get_collider().has_method("die"):
-				ray.get_collider().die()
-
 		"machine":
 			sound_player.stream = preload("res://sound/machine.ogg")
-			sound_player.play()
-			if ray.is_colliding() and ray.get_collider().has_method("die"):
-				ray.get_collider().die()
-
 		"mini":
 			sound_player.stream = preload("res://sound/mini.ogg")
-			sound_player.play()
-			if ray.is_colliding() and ray.get_collider().has_method("die"):
-				ray.get_collider().die()
-
 		"knife":
 			sound_player.stream = preload("res://sound/Knife.wav")
-			sound_player.play()
 
-			if ray.is_colliding():
-				var target = ray.get_collider()
-				var dist = global_position.distance_to(target.global_position)
+	sound_player.play()
 
-				if dist <= KNIFE_RANGE and target.has_method("die"):
-					target.die()
+	if target == null:
+		return
+
+	if Global.current_weapon == "knife":
+		var dist = global_position.distance_to(target.global_position)
+		if dist <= KNIFE_RANGE and target.has_method("die"):
+			target.die()
+	else:
+		if target.has_method("die"):
+			target.die()
 
 func damage():
 	if is_dead:
 		return
 
-	Global.player_health -= 10
+	Global.player_health -= 5
 	print("health = ", Global.player_health)
 
 	if Global.player_health <= 0:
-		is_dead = true
-		Global.player_health = 0
-		print("GAME OVER TRIGGERED")
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		restart_screen.show_game_over()
+		die()
+
+func die():
+	if is_dead:
+		return
+
+	is_dead = true
+	Global.player_health = 0
+
+	print("GAME OVER TRIGGERED")
+
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	restart_screen.show_game_over()
+
+	await get_tree().create_timer(2.0).timeout
+
+	Global.player_health = 100
+	Global.player_score = 0
+
+	get_tree().reload_current_scene()
